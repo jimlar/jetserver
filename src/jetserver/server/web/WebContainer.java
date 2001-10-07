4f22a217c2646c95d44e28707b9497ccc18d8371
jimmy <jimmy>
2001-10-07 09:55:12 +0000

    now we can deploy web apps

---
+++ src/jetserver/server/web/WebContainer.java
@@ -0,0 +1,35 @@
+
+package jetserver.server.web;
+
+import java.io.*;
+import java.util.*;
+
+import jetserver.server.web.services.file.*;
+
+class WebContainer {
+
+    private FileService fileService;
+
+    private String httpRoot;
+    private File fileRoot;
+
+    /**
+     * @param httpRoot the root of this instance the http way
+     * @param fileRoot the directory where whe web-app has been unpacked
+     */
+    public WebContainer(String httpRoot, File fileRoot) throws IOException {
+	this.httpRoot = httpRoot;
+	this.fileRoot = fileRoot;
+	this.fileService = new FileService(fileRoot, new ArrayList());
+    } 
+
+    public String getHttpRoot() {
+	return this.httpRoot;
+    }
+
+    public void service(HttpRequest request, HttpResponse response) 
+	throws IOException
+    {
+	fileService.service(request, response);
+    }
+}
