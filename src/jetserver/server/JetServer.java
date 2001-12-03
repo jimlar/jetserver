d4f2bf65111301920ffd1c108c64ac99109e4791
jimmy <jimmy>
2001-12-03 13:06:54 +0000

    *** empty log message ***

--- src/jetserver/server/JetServer.java
+++ src/jetserver/server/JetServer.java
@@ -10,27 +10,37 @@ import jetserver.util.Log;
 
 public class JetServer {
 
+    private Log log;
     private DropZoneWatch dropZoneWatch;
     private Deployer containerManager;
 
-    public JetServer() 
-	throws IOException
+    public JetServer()
+            throws IOException
     {
-	this.containerManager = new Deployer();
-	this.dropZoneWatch = new DropZoneWatch(containerManager);
-	this.dropZoneWatch.start();
+        this.log = Log.getInstance(this);
+        this.containerManager = new Deployer();
+        this.dropZoneWatch = new DropZoneWatch(containerManager);
+        this.dropZoneWatch.start();
 
-	Log.getInstance(this).info("started");
+        Runtime.getRuntime().addShutdownHook(new ShutdownManager());
+
+        log.info("started");
     }
 
     public static void main(String args[]) throws Exception {
 
-	JetServer jetServer = new JetServer();
-	
-	while (true) {
-	    try {
-		Thread.sleep(3600000);
-	    } catch (InterruptedException e) {}
-	}
+        JetServer jetServer = new JetServer();
+
+        while (true) {
+            try {
+                Thread.sleep(3600000);
+            } catch (InterruptedException e) {}
+        }
+    }
+
+    private class ShutdownManager extends Thread {
+        public void run() {
+            log.getInstance(this).info("shutdown");
+        }
     }
 }
